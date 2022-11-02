ea3edf5456efa1d1c4d3f5b4d180d110073528e9
Gabe S <gabesliwowicz@gmail.com>
2022-11-02 14:43:15 -0400

    User can set the size of the grid by inputting a number over 0 and below 65. The setSide function is called by user input within an input box. The input is then compared in an if statement to ensure it is not negative or the number is not too high. After the global variable for size (squaresOneSide) has been changed. The deleteGrid function is called.

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 let squaresOneSide = 16; // number of squares is the number here to the power of two
 
-const resetBtn = document.querySelector('#reset');
-resetBtn.addEventListener('click', () => deleteGrid());
+const resetBtn = document.querySelector('#reset'); // get reset button element
+resetBtn.addEventListener('click', () => deleteGrid()); // create event on click
 
-makeGrid();
+const setSize = document.querySelector('#size'); // get the size input element
+
+setSize.addEventListener('input', () => setSide()); // give it an event listener
+
+makeGrid(); // makes the grid
 
 // Determines the size of the grid by setting the global variable for the number
 // of squares on one side.
 function setSide()
 {
-    if(input > 0 && !isNaN)
-        squaresOneSide = input;
-
-    deleteGrid();
+    // get the number input
+    let input1 = document.getElementById('size').value;
+    
+    // if number is greater than 0 and under or equal to 64, readjusts
+    if(input1 > 0 && input1 <= 64)
+    {
+        squaresOneSide = input1;
+        deleteGrid();
+    }
 }
 
+// sets the grid to an empty container and then calls the makeGrid function to remake
+// the grid
 function deleteGrid()
 {
     const grid = document.querySelector('.GridContainer');
 
-    grid.innerHTML = '';
+    grid.textContent = '';
 
     makeGrid();
 }
@@ -31,15 +42,16 @@ function makeGrid()
     const square = document.createElement('div'); // create square element
     square.setAttribute('id', 'square'); // give attributes
     square.style.background = 'white';
-    square.style.outline = 'solid'; // better than border when there is overlap
+    square.style.outline = 'solid'; // better than border because there is no overlap
     square.style.flexShrink = '1';
-    square.style.flexGrow = '1';
+    square.style.flexGrow = '1'; // all squares will grow to an equal size in the
+                                // container
 
     for(i = 0; i < squaresOneSide; i++)
     { 
         let row = square.cloneNode(true); // make clone of square and call it row
         row.style.display = 'flex';
-        row.style.flexDirection = 'row';
+        row.style.flexDirection = 'row'; // use flex to set direction
         grid.appendChild(row);
 
         for(i2 = 0; i2 < squaresOneSide; i2++)
