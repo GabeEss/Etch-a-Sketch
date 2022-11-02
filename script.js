c263e1953caa3dbe63c9a822bc74bef8112c4245
Gabe S <gabesliwowicz@gmail.com>
2022-11-02 13:59:37 -0400

    Squares fill when mouse hovers over. Reset button calls deleteGrid and then makeGrid to reset the grid.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let squaresOneSide = 16; // number of squares is the number here to the power of two
 
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', () => deleteGrid());
+
 makeGrid();
 
 // Determines the size of the grid by setting the global variable for the number
@@ -8,6 +11,15 @@ function setSide()
 {
     if(input > 0 && !isNaN)
         squaresOneSide = input;
+
+    deleteGrid();
+}
+
+function deleteGrid()
+{
+    const grid = document.querySelector('.GridContainer');
+
+    grid.innerHTML = '';
 
     makeGrid();
 }
@@ -19,9 +31,7 @@ function makeGrid()
     const square = document.createElement('div'); // create square element
     square.setAttribute('id', 'square'); // give attributes
     square.style.background = 'white';
-    square.style.borderStyle = 'dashed';
-    square.style.borderColor = 'black';
-    square.style.borderWidth = 'thin'
+    square.style.outline = 'solid'; // better than border when there is overlap
     square.style.flexShrink = '1';
     square.style.flexGrow = '1';
 
@@ -37,6 +47,10 @@ function makeGrid()
             let column = square.cloneNode(true);
             column.style.display='flex';
             column.style.flexDirection='column';
+
+            // Add event listener to fill in each of the squares if the mouse moves over
+            column.addEventListener('mouseover', () => column.style.background = 'black');
+
             row.appendChild(column);
         }
     }
